6d7850a8506be775c93381da7ed956f5b2e62acf
thomas <thomasfong102@gmail.com>
2024-12-01 06:28:28 +0800

    installed swagger

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,13 @@ const router = express.Router();
 const usersController = require('../controllers/users');
 
 router .get('/', usersController.getAll);
-router.get('/', usersController.getSingle);
+
+router.get('/:id', usersController.getSingle);
+
+router.post('/', usersController.createUser);
+
+router.put('/:id', usersController.updateUser);
+
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
