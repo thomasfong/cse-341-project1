98269849819a2733f3dca1da3b5b9bb3984e14ac
thomas <thomasfong102@gmail.com>
2024-12-02 21:56:12 +0800

    update

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Contacts Api',
         description: 'Contacts Api',
     },
-    host: 'localhost:3001/',
+    host: 'localhost:3001',
     schemes: ['http','https']
 };
 
